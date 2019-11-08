1dcb1ddfeaef94e14278a144080f57e6e2dc8cf7
nawab-shah <57325543+nawab-shah@users.noreply.github.com>
2019-11-08 14:32:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('#btn').click(function(){ 
+      var city = $('#city').val();
+      //alert(city);
+      $('#temp').attr('style','"display: block;"')     
+      //$('#result').attr('src',`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=d04a2f7e083913ea5b3767657fbc6a38`);
+      
+      var tempr = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d04a2f7e083913ea5b3767657fbc6a38`,
+        "method": "GET"
+      }
+      
+      $.ajax(tempr).done(function (response) {
+      
+        $(".temprature").empty();
+        var content = response.main.temp;
+        $(".temprature").append(content);
+      
+        $(".humid").empty();
+        var humidity = response.main.humidity;
+        $(".humid").append(humidity);
+      
+      });
+
+    });
+  });
+  
